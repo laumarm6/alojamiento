09e1a9cc3fcc564356018a930f2b324a23f9bef4
Laura Marti <laumarmar15@alu.edu.gva.es>
2025-04-07 16:51:58 +0000

    ajustes iniciales

--- __manifest__.py
+++ __manifest__.py
@@ -22,9 +22,11 @@
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
+    'application': True,
+
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
