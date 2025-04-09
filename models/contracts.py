49f58c6fdd9c7db019e1e407c97d44c52928b23d
Laura Marti <laumarmar15@alu.edu.gva.es>
2025-04-09 17:44:03 +0000

    cambio todos los nombres a 'contracts'

--- models/contracts.py
+++ models/contracts.py
@@ -1,8 +1,8 @@
 from odoo import models, fields
 
 
-class Contratos(models.Model):
-    _name = 'alojamiento.contratos'
+class Contracts(models.Model):
+    _name = 'alojamiento.contracts'
     _description = 'Define cada uno de los alojamientos para estudiantes'
 
     name = fields.Char(string="Nombre")
