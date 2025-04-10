9b6175d721769e7cd01773cab5552ceef26490d3
Laura Marti <laumarmar15@alu.edu.gva.es>
2025-04-10 18:03:02 +0000

    cambio el orden de los menus

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from . import models
-from . import contracts
-from . import landlords
-from . import accommodations
-from . import maintenanceissues
 from . import allocations
+from . import accommodations
 from . import rooms
+from . import maintenanceissues
+from . import landlords
+from . import contracts
+
+
+
+
