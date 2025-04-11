b4f877844ac3c9fa8e97580d2832b5c03a634294
Laura Marti <laumarmar15@alu.edu.gva.es>
2025-04-11 17:31:04 +0000

    amplio modelo 'Contratos' con restricciones

--- models/contracts.py
+++ models/contracts.py
@@ -1,11 +1,24 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
+import re
 
 class Contracts(models.Model):
     _name = 'alojamiento.contracts'
     _description = 'Define cada uno de los alojamientos para estudiantes'
 
-    name = fields.Char(string="Nombre")
-    status = fields.Char()
+    name = fields.Char(string="Identificador contrato", size = 10)
+    status = fields.Boolean()
 
-    
+
+    _sql_constraints = [
+    ('name_uniq', 'unique(name)', 'El identificador debe ser unico'),
+]
+
+    @api.constrains('name')
+    def _check_identificador(self):
+        for record in self:
+            pattern = re.compile("^[A-Z]{3}\d{5,}$")
+            if not pattern.match(record.name):
+                raise ValidationError('El formato deben ser AAANNNNN donde A es una letra mayúsca y N un número')
+        
