04b18c359306f5f3a1a3daa6bd1ce57dcd297b31
Laura Marti <laumarmar15@alu.edu.gva.es>
2025-04-13 18:29:34 +0000

    añado automatizacion fecha baja en 'incidencias'

--- models/maintenanceissues.py
+++ models/maintenanceissues.py
@@ -1,7 +1,9 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+from datetime import date
 
 import re
+
 
 
 class MaintenanceIssues(models.Model):
@@ -9,6 +11,8 @@ class MaintenanceIssues(models.Model):
     _description = 'Define cada las incidencias asociadas a los alojameintos'
 
     name = fields.Char(string="Identificador incidencia", size = 10, required = True)
+    start_date= fields.Date(string="Fecha de alta", default=lambda self: date.today())
+    end_date= fields.Date(string="Fecha de baja")
     status = fields.Selection([('PEN','Pendiente'),('PRO','En proceso'),('RES', 'Resuelta')])
 
 
@@ -23,4 +27,10 @@ class MaintenanceIssues(models.Model):
             pattern = re.compile("\d{3,}")
             if not pattern.match(record.name):
                 raise ValidationError('El formato deben ser NNN donde N un número')
-        
+
+#regsitra la fecha de baja cuando cambia el status
+
+    @api.onchange('status')
+    def _onchange_status(self):
+        if self.status == 'Resuleta':
+            self.fecha_baja = date.now()
