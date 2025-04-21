5bd81f9e20d268aa60a169c6132fc3f75911f3c4
Laura Marti <laumarmar15@alu.edu.gva.es>
2025-04-21 16:55:12 +0000

    añado herencia de clase a modelo Clientes de Oddoo

--- models/aclients.py
+++ models/aclients.py
@@ -2,8 +2,18 @@ from odoo import models, fields
 
 
 class AccommodationClients(models.Model):
-    _name = 'alojamiento.aclients'
-    _description = 'Listado de los cleintes con alojamiento y sus características'
 
-    name = fields.Char(string="Nombre")
-    status = fields.Char()
+    # herencia por propotipo extiendo la clase pero creo nuevas vistas
+    #_name = 'alojamiento.aclients'
+    _description = 'Listado de los clientes con alojamiento y sus características'
+    _inherit = 'res.partner'
+
+  # room_preference = fields.Selection([
+       # ('SGL', 'Individual'),
+       # ('DBL', 'Doble'),
+        #('PRV', 'Con baño privado'),
+   # ], string="Preferencia de habitación")
+   # accommodation_client = fields.Boolean(string="Cliente de Alojamiento", default=False)
+
+   # name = fields.Char(string="Nombre")
+    #status = fields.Char()  
